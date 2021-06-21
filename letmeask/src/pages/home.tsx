d6bbf985cecc9a0bf488b9f41c9141256608fa62
cristianokoxne <57273233+cristianokoxne@users.noreply.github.com>
2021-06-21 20:16:51 -0300

    update

---
+++ letmeask/src/pages/home.tsx
@@ -0,0 +1,12 @@
+import illustration from '..//assets/images/illustration.svg'
+
+export function Home(){
+    return (
+        <div>
+            <aside>
+                <img src={illustration} alt="ilustração simbolizando perguntas e respostas" />
+            </aside>
+
+        </div>
+    )
+}
