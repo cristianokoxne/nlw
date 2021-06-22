975abaf9403ab4b973dd31473c0f8a5259b4f56b
cristianokoxne <57273233+cristianokoxne@users.noreply.github.com>
2021-06-22 00:45:40 -0300

    update

--- letmeask/src/App.tsx
+++ letmeask/src/App.tsx
@@ -1,8 +1,18 @@
+
+
+import {BrowserRouter, Route }from 'react-router-dom'
+
+import { NewRoom } from "./pages/NewRoom";
 import { Home } from "./pages/home";
 
 function App() {
   return (
-    <Home/>
+    <BrowserRouter>
+      <Route path ="/" exact component ={Home}></Route>
+      <Route path ="/rooms/new" component ={NewRoom}></Route>
+      
+    
+    </BrowserRouter>
   );
 }
 
